ff9a2e0d45c8e9a66fd3aff831e129371ec20281
Jono <jonotempler@gmail.com>
2017-03-09 21:19:42 +0200

    assert errors

--- test/xmlAttr.js
+++ test/xmlAttr.js
@@ -2,17 +2,18 @@ var assert = require('assert');
 var gverify = require('../index');
 
 var testOptions = {
-    file: '<xml><file attr="test"/></xml>'
+    file: '<xml><js attr="test"/><css attr="test"/></xml>',
+    xpaths: ['/xml/js/@attr','/xml/css/@attr']
 };
 
 describe('xmlAttr', () => {
     it('xml attribute contains value', () => {
-        const result = gverify.xmlAttr('', '/xml/file/@attr', 'test', testOptions);
+        const result = gverify.xmlAttr('', testOptions.xpaths, 'test', testOptions);
         assert.equal(result, true);
     });
 
     it('xml attribute not contains value', () => {
-        const result = gverify.xmlAttr('', '/xml/file/@attr', 'fail', testOptions);
+        const result = gverify.xmlAttr('', testOptions.xpaths, 'fail', testOptions);
         assert.equal(result, false);
     });
 });
